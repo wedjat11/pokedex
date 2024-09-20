9c1ead921253609060774a115662df531c037c0c
wedjat11 <jamontiel@ammper.com>
2024-09-20 13:56:12 -0600

    feat: mejora en servicio

--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -1,25 +1,48 @@
-// src/services/pokemonService.js
 import { Pokemon } from '../models/pokemonModel';
-
-export const fetchPokemons = async (page) => {
+export const fetchPokemons = async (page, signal) => {
   const offset = (page - 1) * 20;
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`);
-  const data = await response.json();
 
-  return Promise.all(
-    data.results.map(async (pokemon) => {
-      const res = await fetch(pokemon.url);
-      const pokeDetails = await res.json();
-      const types = pokeDetails.types.map(typeInfo => typeInfo.type.name);
-      console.log(types);
-      return new Pokemon(
-        pokeDetails.id,
-        pokeDetails.name,
-        pokeDetails.sprites.front_shiny || pokeDetails.sprites.front_default,
-        types,
-        pokeDetails.stats
-      );
-      
-    })
-  );
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=${offset}`, { signal });
+    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const detailedPokemons = await Promise.all(
+      data.results.map(async (pokemon) => {
+        try {
+          const res = await fetch(pokemon.url, { signal });
+          
+          if (!res.ok) {
+            throw new Error(`Error fetching details for ${pokemon.name}: ${res.status}`);
+          }
+
+          const pokeDetails = await res.json();
+          const types = pokeDetails.types.map(typeInfo => typeInfo.type.name);
+
+          return new Pokemon(
+            pokeDetails.id,
+            pokeDetails.name,
+            pokeDetails.sprites.front_shiny || pokeDetails.sprites.front_default,
+            types,
+            pokeDetails.stats
+          );
+        } catch (error) {
+          console.error(`Failed to fetch details for ${pokemon.name}:`, error);
+          return null; 
+        }
+      })
+    );
+    return detailedPokemons.filter(pokemon => pokemon !== null);
+  } catch (error) {
+    if (error.name === 'AbortError') {
+      console.log('Request aborted');
+    } else {
+      console.error('Failed to fetch Pokémon list:', error);
+    }
+    return []; 
+  }
 };
